7ebb26776b50a4a1b7135eb753ee4701776fed71
Wallace Honorato da Silva <101594950+WallaceHS20@users.noreply.github.com>
2023-05-29 20:25:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Paciente {
-    int id;
-    struct Paciente *proximo;
-} Paciente;
+typedef struct Aluno {
+    int matricula;
+    struct Aluno *proximo;
+} Aluno;
 
-void enfileiraPaciente(int id, Paciente **comeco, Paciente **fim) {
-    Paciente novo = (Paciente) malloc(sizeof(Paciente));
+void enfileiraAluno(int matricula, Aluno **comeco, Aluno **fim) {
+    Aluno *novo = (Aluno*)malloc(sizeof(Aluno));
     if (novo == NULL) {
         printf("Erro de alocação do nó.\n");
     } else {
-        novo->id = id;
+        novo->matricula = matricula;
         novo->proximo = NULL;
         if (*comeco == NULL) {
             *comeco = novo;
         } else {
-            *(fim)->proximo = novo;
+            (*fim)->proximo = novo;
         }
         *fim = novo;
     }
 }
 
-int desenfileiraPaciente(Paciente **comeco, Paciente **fim) {
-    Paciente *auxiliar = *comeco;
-    int lixo;
+int desenfileiraAluno(Aluno **comeco, Aluno **fim) {
+    Aluno *auxiliar = *comeco;
+    int matricula;
     if (auxiliar != NULL) {
         *comeco = auxiliar->proximo;
-        lixo = auxiliar->id;
+        matricula = auxiliar->matricula;
         auxiliar->proximo = NULL;
         free(auxiliar);
         if (*comeco == NULL) {
             *fim = NULL;
         }
-        return lixo;
+        return matricula;
     } else {
         printf("Fila vazia\n");
         return -1;
     }
 }
 
-void imprimeFilaPacientes(Paciente **comeco) {
-    Paciente *paciente = *comeco;
-    if (paciente != NULL) {
-        while (paciente != NULL) {
-            printf("ID: %d", paciente->id);
-            paciente = paciente->proximo;
+void imprimeFilaAlunos(Aluno **comeco) {
+    Aluno *aluno = *comeco;
+    if (aluno != NULL) {
+        while (aluno != NULL) {
+            printf("Matricula: %d\n", aluno->matricula);
+            aluno = aluno->proximo;
         }
     } else {
         printf("Fila vazia\n");
@@ -53,37 +53,36 @@ void imprimeFilaPacientes(Paciente **comeco) {
 }
 
 int main() {
-    Paciente *comeco = NULL;
-    Paciente *fim = NULL;
-    int opcao, id;
+    Aluno *comeco = NULL;
+    Aluno *fim = NULL;
+    int opcao, matricula;
 
     do {
         printf("Escolha uma opção:\n");
-        printf("1. Adicionar paciente na fila\n");
-        printf("2. Atender próximo paciente\n");
-        printf("3. Imprimir fila de pacientes\n");
+        printf("1. Adicionar aluno na fila\n");
+        printf("2. Atender próximo aluno\n");
+        printf("3. Imprimir fila de alunos\n");
         printf("4. Sair\n");
         scanf("%d", &opcao);
 
         switch (opcao) {
             case 1:
-                printf("Digite o ID do paciente: ");
-                scanf("%d", &id);
-                enfileiraPaciente(id, &comeco, &fim);
-                printf("Paciente adicionado na fila.\n");
+                printf("Digite a matricula do aluno: ");
+                scanf("%d", &matricula);
+                enfileiraAluno(matricula, &comeco, &fim);
+                printf("Aluno adicionado na fila.\n");
                 break;
             case 2: {
-                int pacienteId = desenfileiraPaciente(&comeco, &fim);
-                if (pacienteId != -1) {
-                    printf("Paciente com ID %d atendido.\n", pacienteId);
-                }
-                else{
-                    printf("Sem paciente para atender.\n");
+                int alunoMatricula = desenfileiraAluno(&comeco, &fim);
+                if (alunoMatricula != -1) {
+                    printf("Aluno com matricula %d atendido.\n", alunoMatricula);
+                } else {
+                    printf("Sem aluno para atender.\n");
                 }
                 break;
             }
             case 3:
-                imprimeFilaPacientes(&comeco);
+                imprimeFilaAlunos(&comeco);
                 break;
             case 4:
                 printf("Saindo...\n");
