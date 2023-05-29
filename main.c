5db7ba8645632f06cb20b0107d93142639eac09a
Wallace Honorato da Silva <101594950+WallaceHS20@users.noreply.github.com>
2023-05-28 22:01:05 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,96 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no{
-    int num;
-    struct no *proximo;
-}No;
+typedef struct Paciente {
+    int id;
+    struct Paciente *proximo;
+} Paciente;
 
-int lernum() {
-    int numero;
-    printf("Digite um número: ");
-    scanf("%d", &numero);
-    return numero;
-}
-
-void imprimirNumero(int num){
-    printf("\nO número digitado foi: %d\n", num);
-}
-
-/** -------------------------------- Função Empilhar ------------------------------------*/
- 
-No* empilhar(No *topo){
-    No *novo = malloc(sizeof(No));
-    
-    if(novo){
-        novo-> num = lernum();
-        novo-> proximo = topo;
-        return novo;
-    }
-    
-    else{
-        printf("\nErro ao alocar memória!!!\n");
-    }
-    
-}
-
-/** -------------------------------- Função Desempilhar ------------------------------------*/   
-
-No* desempilhar (No **topo){
-    if(*topo != NULL){
-        No *remover = *topo;
-        *topo = remover-> proximo;
-        return  remover;
+void enfileiraPaciente(int id, Paciente **comeco, Paciente **fim) {
+    Paciente novo = (Paciente) malloc(sizeof(Paciente));
+    if (novo == NULL) {
+        printf("Erro de alocação do nó.\n");
+    } else {
+        novo->id = id;
+        novo->proximo = NULL;
+        if (*comeco == NULL) {
+            *comeco = novo;
+        } else {
+            *(fim)->proximo = novo;
+        }
+        *fim = novo;
     }
 }
 
-/** -------------------------------- Função Imprimir ------------------------------------*/   
-
-void imprimirPilha(No *topo){
-    printf("\n---------- PILHA ----------\n");
-    
-    while(topo){
-        imprimirNumero(topo->num);
-        topo = topo-> proximo;
+int desenfileiraPaciente(Paciente **comeco, Paciente **fim) {
+    Paciente *auxiliar = *comeco;
+    int lixo;
+    if (auxiliar != NULL) {
+        *comeco = auxiliar->proximo;
+        lixo = auxiliar->id;
+        auxiliar->proximo = NULL;
+        free(auxiliar);
+        if (*comeco == NULL) {
+            *fim = NULL;
+        }
+        return lixo;
+    } else {
+        printf("Fila vazia\n");
+        return -1;
     }
-    
-    printf("\n--------FINAL PILHA -------\n");
 }
 
+void imprimeFilaPacientes(Paciente **comeco) {
+    Paciente *paciente = *comeco;
+    if (paciente != NULL) {
+        while (paciente != NULL) {
+            printf("ID: %d", paciente->id);
+            paciente = paciente->proximo;
+        }
+    } else {
+        printf("Fila vazia\n");
+    }
+}
 
-int main()
-{
-    No *remover, *topo = NULL;
-    int opc;
-    
-    do{
-        printf("\n0 - Sair\n1 - Empilhar\n2 - Desempilhar\n3 - Imprimir\nSelecione uma opção: ");
-        scanf("%d", &opc);
-        
-        switch (opc){
-            
+int main() {
+    Paciente *comeco = NULL;
+    Paciente *fim = NULL;
+    int opcao, id;
+
+    do {
+        printf("Escolha uma opção:\n");
+        printf("1. Adicionar paciente na fila\n");
+        printf("2. Atender próximo paciente\n");
+        printf("3. Imprimir fila de pacientes\n");
+        printf("4. Sair\n");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
             case 1:
-            
-                topo = empilhar(topo);
-                
+                printf("Digite o ID do paciente: ");
+                scanf("%d", &id);
+                enfileiraPaciente(id, &comeco, &fim);
+                printf("Paciente adicionado na fila.\n");
                 break;
-                
-            case 2:
-            
-                remover = desempilhar(&topo);
-                
-                if (remover){
-                    printf("\nElemento removido com sucesso!\n");
-                    imprimirNumero(remover->num);
+            case 2: {
+                int pacienteId = desenfileiraPaciente(&comeco, &fim);
+                if (pacienteId != -1) {
+                    printf("Paciente com ID %d atendido.\n", pacienteId);
                 }
-                
+                else{
+                    printf("Sem paciente para atender.\n");
+                }
                 break;
-                
+            }
             case 3:
-            
-                imprimirPilha(topo);
-        } 
-        
-    } while (opc != 0);
-    
+                imprimeFilaPacientes(&comeco);
+                break;
+            case 4:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+                break;
+        }
+
+        printf("\n");
+    } while (opcao != 4);
+
+    return 0;
 }
